f60f4bb177d6d2f6ad35b9802cad5d811d9b1379
Thang Truong <tcthang.dsna@gmail.com>
2015-11-20 09:56:00 +0700

    add app_public_key

--- CheckingLicense/app/src/main/java/com/at/checkinglicense/MainActivity.java
+++ CheckingLicense/app/src/main/java/com/at/checkinglicense/MainActivity.java
@@ -1,15 +1,31 @@
 package com.at.checkinglicense;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.at.checkinglicense.license.AESObfuscator;
+import com.at.checkinglicense.license.LicenseChecker;
+import com.at.checkinglicense.license.LicenseCheckerCallback;
+import com.at.checkinglicense.license.ServerManagedPolicy;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Generate your own 20 random bytes, and put them here.
+    private static final byte[] SALT = new byte[]{
+            -46, 65, 30, -128, -103, -57, 74, -64, 51, 88, -95, -45, 77, -117, -36, -113, -11, 32, -64,
+            89
+    };
+
+    private LicenseCheckerCallback mLicenseCheckerCallback;
+    private LicenseChecker mChecker;
+
+    private TextView mTxtStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +42,69 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Try to use more data here. ANDROID_ID is a single point of attack.
+        String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        // Library calls this when it's done.
+        mLicenseCheckerCallback = new MyLicenseCheckerCallback();
+        // Construct the LicenseChecker with a policy.
+        mChecker = new LicenseChecker(
+                this, new ServerManagedPolicy(this,
+                new AESObfuscator(SALT, getPackageName(), deviceId)),
+                getString(R.string.app_public_key));
+
+        mTxtStatus = (TextView) findViewById(R.id.txtStatus);
+        findViewById(R.id.btnCheckingLicense).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mChecker.checkAccess(mLicenseCheckerCallback);
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private class MyLicenseCheckerCallback implements LicenseCheckerCallback {
+        public void allow(int policyReason) {
+            if (isFinishing()) {
+                // Don't update UI if Activity is finishing.
+                return;
+            }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            mTxtStatus.setText("License OK");
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        public void dontAllow(int policyReason) {
+            if (isFinishing()) {
+                // Don't update UI if Activity is finishing.
+                return;
+            }
+
+            mTxtStatus.setText("License not OK");
+
+            // Should not allow access. In most cases, the app should assume
+            // the user has access unless it encounters this. If it does,
+            // the app should inform the user of their unlicensed ways
+            // and then either shut down the app or limit the user to a
+            // restricted set of features.
+            // In this example, we show a dialog that takes the user to Market.
+            // If the reason for the lack of license is that the service is
+            // unavailable or there is another problem, we display a
+            // retry button on the dialog and a different message.
+        }
+
+        public void applicationError(int errorCode) {
+            if (isFinishing()) {
+                // Don't update UI if Activity is finishing.
+                return;
+            }
+
+            mTxtStatus.setText("Error " + errorCode);
+
+            // This is a polite way of saying the developer made a mistake
+            // while setting up or calling the license checker library.
+            // Please examine the error code and fix the error.
+        }
     }
+
 }
